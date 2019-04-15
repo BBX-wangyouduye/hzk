f59f6fd4fca8dbd338a0d5e5a2952ebf93f07080
BBX-wangyouduye <wqx628262@163.com>
2019-04-15 09:01:21 +0800

    登陆布局

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import AppRouter from './router'
+// import 'antd-mobile/dist/antd-mobile.css'
 class App extends Component {
   render() {
     return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
-
+import { Flex, NavBar, List, InputItem, Button,WingBlank } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css'
+import './Login.css'
 class Login extends Component {
   constructor(props) {
     super(props)
@@ -8,7 +10,21 @@ class Login extends Component {
   }
   render() {
     return (
-      <div>Login---</div>
+      <div>
+        <WingBlank size="sm">
+          <Flex direction="column" justify="center">
+            <Flex.Item><NavBar mode="light">登录</NavBar></Flex.Item>
+            <Flex.Item>
+              <List>
+                <InputItem>姓名</InputItem>
+                <InputItem>密码</InputItem>
+              </List>
+              <Button type="primary">登录</Button>
+            </Flex.Item>
+          </Flex>
+        </WingBlank>
+      </div>
+
     )
   }
 }
